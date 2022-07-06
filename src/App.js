2603a7dce69672001ba6c8950229278e160904f8
Nahuel <Soccernahu@gmail.com>
2022-07-05 23:38:15 -0300

    Desafio Coder House 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import React from 'react';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,39 @@
-function NavBar() {
+import React from "react"
+
+
+const NavBar = () => {
   return (
-    <div style={{backgroundColor:'black', height:'40px', padding:'8px' }}> <button className="logo" style={ { backgroundColor:'white', marginRight:'80%' }}>Product Design</button> 
-    <button className="menu" style={ { backgroundColor:'white'}}><select name="menu" id="menu">
-  <option value="1">Menù</option>
-  <option value="2">Productos</option>
-  <option value="3">Nosotros</option>
-  <option value="4">Contactenos</option>
-</select></button>
-    
-    </div>
-)
+    <nav class="navbar bg-dark">
+      <div class="container-fluid">
+        <button className="logo" class="btn btn-light">Product Design</button>
+        <select class="btn btn-light" name="menu" id="menu">
+          <option value="1">Menù</option>
+          <option value="2">Productos</option>
+          <option value="3">Nosotros</option>
+          <option value="4">Contactenos</option>
+        </select>
+      </div>
+    </nav>
+  )
 }
 
-export default 
+export default
 
-NavBar
+  NavBar
+
+// < nav class="navbar bg-light" >
+//     <div class="container-fluid">
+//       <a class="navbar-brand">Product Design</a>
+//       <form class="d-flex" >
+//           <button class="btn btn-outline-success" type="submit">Search</button>
+//       </form>
+//     </div>
+// </nav >
+{/* <nav class="navbar bg-light" style={{ backgroundColor: 'black', height: '40px', padding: '8px' }}> <div class="container-fluid"> <button className="logo" style={{ backgroundColor: 'white', marginRight: '80%' }}>Product Design</button>
+      <button className="menu" style={{ backgroundColor: 'white' }}><select name="menu" id="menu">
+        <option value="1">Menù</option>
+        <option value="2">Productos</option>
+        <option value="3">Nosotros</option>
+        <option value="4">Contactenos</option>
+      </select></button>
+    </div> */}
