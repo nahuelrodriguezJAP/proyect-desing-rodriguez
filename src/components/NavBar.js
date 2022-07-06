96816ffa486a9bed20b4255db508a69df4834abf
Nahuel <Soccernahu@gmail.com>
2022-07-05 22:09:27 -0300

    desafio 5/7

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 function NavBar() {
   return (
     <div style={{backgroundColor:'black', height:'40px', padding:'8px' }}> <button className="logo" style={ { backgroundColor:'white', marginRight:'80%' }}>Product Design</button> 
-    <button className="menu" style={ { backgroundColor:'white'}}>Menù</button>
+    <button className="menu" style={ { backgroundColor:'white'}}><select name="menu" id="menu">
+  <option value="1">Menù</option>
+  <option value="2">Productos</option>
+  <option value="3">Nosotros</option>
+  <option value="4">Contactenos</option>
+</select></button>
     
     </div>
 )
