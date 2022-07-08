65de460cae6eb732b9367dcd3bb55b4dc4ebb8ab
Nahuel <Soccernahu@gmail.com>
2022-07-07 23:25:41 -0300

    desafio 7/7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import React from 'react';
+import ItemListContainer from './container/ItemListContainer';
+// import ItemCard from './container/ItemCard'
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <ItemListContainer nombre='nombre'/>
     </div>
   );
 }
